da4f3df1f25fd4e1b21541ebd14b4ed9479c5e16
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-10-23 16:20:17 -0600

    added event listeners for adding info

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ function UI() {}
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", 
     function(e) {
-        console.log("test");
+        const title = document.getElementById("title").value,
+              author = document.getElementById("author").value,
+              isbn = document.getElementById("isbn").value
+
+              console.log(title, author, isbn);
 
         e.preventDefault();
     }
