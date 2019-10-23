de4bf1da9d1d4afa2cd699a8aa4b388aee046a11
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-10-23 16:45:51 -0600

    made it so fields clear after book and author is added

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row);
 }
 
+// Clear Fields
+UI.prototype.clearFields = function() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+}
+
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", 
     function(e) {
@@ -41,6 +48,9 @@ document.getElementById("book-form").addEventListener("submit",
         
         // Add book to list 
         ui.addBookToList(book);  
+
+        // Clear fields
+        ui.clearFields()
 
         e.preventDefault();
     }
