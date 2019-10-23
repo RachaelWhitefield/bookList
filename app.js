94869878f2f79aad59503b45dcdc9d5d54832661
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-10-23 16:15:44 -0600

    added html for UI

---
+++ app.js
@@ -0,0 +1,19 @@
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+ 
+
+// UI Constructor
+function UI() {}
+
+// Event Listeners
+document.getElementById("book-form").addEventListener("submit", 
+    function(e) {
+        console.log("test");
+
+        e.preventDefault();
+    }
+);
